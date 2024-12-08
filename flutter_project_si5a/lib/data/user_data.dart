e6e0d13f56d6edcf31728ce9259468b9a8f312e2
ericcandra <ericcandra_2226240059@mhs.mdp.ac.id>
2024-12-08 13:20:50 +0700

    update

---
+++ flutter_project_si5a/lib/data/user_data.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_project_si5a/models/user.dart';
+
+var userList = [
+  User(name: 'ericcandra',email: 'ericcandra@gmail.com',password: '12345'),
+  User(name: 'nabila',email: 'nabila@gmail.com',password: '12345'),
+];
--- flutter_project_si5a/lib/screens/home_screens.dart
+++ flutter_project_si5a/lib/screens/home_screens.dart
@@ -1,16 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project_si5a/data/home_data.dart';
+import 'package:flutter_project_si5a/models/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomeScreen extends StatelessWidget {
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<void> _logout(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('isLoggedIn');
+    await prefs.remove('email');
+    Navigator.pushReplacementNamed(context, '/login');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
+         actions: [
+          IconButton(onPressed: (){_logout(context);}, icon: const Icon(Icons.logout))
+        ],
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16),child: Text('ini adalah halaman home')),
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, crossAxisSpacing: 8, mainAxisSpacing: 8),
+              padding: const EdgeInsets.all(8),
+              itemCount: homeList.length,
+              itemBuilder: (context, index) {
+                Home varHome = homeList[index];
+                return Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16)),
+                    margin: const EdgeInsets.all(6),
+                    elevation: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        // Gambar Tempat
+                        Expanded(
+                            child: ClipRRect(
+                          borderRadius: BorderRadius.circular(16),
+                          child: Image.asset(
+                            varHome.imageAsset,
+                            fit: BoxFit.cover,
+                          ),
+                        )),
+                        // Nama Tempat
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16, top: 8),
+                          child: Text(
+                            varHome.name,
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        // Lokasi Tempat
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16, bottom: 8),
+                          child: Text(
+                            varHome.location,
+                            style: const TextStyle(fontSize: 12),
+                          ),
+                        ),
+                      ],
+                    ));
+              },
+            )
+          ],
+        ),
+      )),
     );
   }
 }
